2f61e6b363cb792c05bb8a511c5e51f33ce6db0a
Martin Dezsi <dezsimartin@gmail.com>
2020-01-12 14:22:09 +0100

    added common components (header,footer, content), init game component. modified css files, added material imports to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 
+import { MatCardModule, MatGridListModule, MatToolbarModule, MatButtonModule } from "@angular/material/";
+
 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ApiCommunicationService } from './services/api-communication.service';
+import { ContentComponent } from './components/common/content/content.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { GameComponent } from './components/game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    HeaderComponent,
+    FooterComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
@@ -19,9 +30,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    ApiCommunicationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
